05c949c9a5c251aeb52c8952f6a0cede489734d0
shihabhassannaim <hassanshihab231@gmail.com>
2023-10-14 20:44:14 +0600

    Day-34

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 const Feed = () => {
     return (
-        <div>
-            
-        </div>
+    <Stack sx={{flexDirection:{sx: 'column' , md : 'row'}}}>
+        <Box sx={{ height: {sx: 'auto' , md:'92vh'}, borderRight : '1px solid #3d3d3d' , px : {sx: 0 , md: 2}
+    }}>
+        SideBar
+    <Typography className='copyright' variant='body2' sx={{mt: 1.5 , color: '#fff' }}>
+        Copyright 2023 Shihab Hassan Naim
+    </Typography>
+        </Box>
+    </Stack>
     );
 };
 
